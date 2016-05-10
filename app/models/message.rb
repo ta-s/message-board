efdf8a32a3bbe618faad71548280618675613186
Takahiro Sasaki <t-sasaki@digital-knowledge.co.jp>
2016-05-10 07:02:48 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
+  validates :age  , numericality: { only_integer: true, greater_than_or_equal_to: 0 }
      # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
